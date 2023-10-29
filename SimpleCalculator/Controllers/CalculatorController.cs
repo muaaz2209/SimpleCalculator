786a98a50232dfb1e7558c3e7180a754466061b1
muaaz2209 <muaazabowath@gmail.com>
2023-10-29 12:57:13 +0200

    Add project files.

---
+++ SimpleCalculator/Controllers/CalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace SimpleCalculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(string expression)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                var result = dt.Compute(expression, "");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Invalid Expression");
+            }
+        }
+    }
+}
